e376ac93747b387c047483540f2dfff26fd25db9
Alexandra <2483859s@student.gla.ac.uk>
2022-09-09 22:10:40 +0100

    resume update

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,9 @@
+<?php
+if(isset($_POST['submit'])){
+    $to = "strompovaa@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+
+  //  $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
