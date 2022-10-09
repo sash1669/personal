8d3bdf1ff92873bafc9dd8c0007c8e6b9be995f1
Alexandra <2483859s@student.gla.ac.uk>
2022-10-09 21:18:12 +0100

    contact form from stackoverflow

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,23 @@
 <?php
+//from Stackoverflow: https://stackoverflow.com/questions/18379238/send-email-with-php-from-html-form-on-submit-with-the-same-script
+if(isset($_POST['submit'])){
+    $to = "strompovaa@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $subject2 = "Copy of your form submission";
+    $message = $name " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -7,6 +26,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
+  /*
   $receiving_email_address = 'strompovaa@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -22,7 +42,7 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-
+*/
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
@@ -32,10 +52,12 @@
     'port' => '587'
   );
   */
-
+/*
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 500);
 
   echo $contact->send();
+  */
+
 ?>
